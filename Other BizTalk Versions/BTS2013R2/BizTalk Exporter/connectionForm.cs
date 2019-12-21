86c24c5ff27588c9743d8e69923c67c6a94ed0f4
Sandro Pereira <sandro-pereira@live.com.pt>
2019-12-21 23:11:25 +0000

    20013 R2

---
+++ Other BizTalk Versions/BTS2013R2/BizTalk Exporter/connectionForm.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Configuration;
+using System.Windows.Forms;
+
+namespace BizTalk_Exporter
+{
+    public partial class connectionForm : Form
+    {
+        public connectionForm()
+        {
+            InitializeComponent();
+        }
+
+        private void confirmBtn_Click(object sender, EventArgs e)
+        {
+            var configFile = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+            var settings = configFile.AppSettings.Settings;
+            settings["connString"].Value = connTxt.Text;
+
+            configFile.Save(ConfigurationSaveMode.Modified);
+            ConfigurationManager.RefreshSection(configFile.AppSettings.SectionInformation.Name);
+            Close();
+        }
+
+        private void cancelBtn_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void connectionForm_Load(object sender, EventArgs e)
+        {
+            var configs = ConfigurationManager.AppSettings;
+            connTxt.Text = configs["connString"];
+        }
+    }
+}
+
---
+++ Other BizTalk Versions/BTS2013R2/BizTalk Exporter/excelData.cs
@@ -0,0 +1,49 @@
+﻿using OfficeOpenXml;
+using System;
+using System.Collections.Generic;
+
+namespace BizTalk_Exporter
+{
+    public class excelData
+    {
+        public string portName;
+        public string portURI;
+        public excelData(string _portName, string _portURI)
+        {
+            portName = _portName;
+            portURI = _portURI;
+        }
+    }
+    public class ExcelOperations
+    {
+        /// <summary>
+        /// Reads all the cells in the given Excel File.
+        /// </summary>
+        /// <param name="environment">QA or PROD</param>
+        /// <param name="excelFile"></param>
+        /// <returns></returns>
+        public List<excelData> ReadExcelFile(string environment, string excelFile)
+        {
+            try
+            {
+                var package = new ExcelPackage(new System.IO.FileInfo(excelFile));
+                ExcelWorksheet sheet = package.Workbook.Worksheets[environment];
+                if (sheet == null)
+                    throw new Exception("Could not find Sheet");
+                int startRow = sheet.Dimension.Start.Row;
+                int endRow = sheet.Dimension.End.Row;
+                List<excelData> portsList = new List<excelData>();
+                for (int i = startRow; i <= endRow; i++)
+                {
+                    portsList.Add(new excelData(
+                        sheet.Cells[i, 1].Text,
+                        sheet.Cells[i, 5].Text
+                    ));
+                }
+                return portsList;
+            }
+            catch (Exception ex)
+            { throw ex; }
+        }
+    }
+}
